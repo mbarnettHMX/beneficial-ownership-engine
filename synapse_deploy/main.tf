e052b4d84eb6c73afda4016e96a88f000e7fee62
ifourajay <ajay@ifourtechnolab.com>
2024-05-27 03:25:57 -0700

    Updating terrafomr files to automate landing container

--- synapse_deploy/main.tf
+++ synapse_deploy/main.tf
@@ -60,6 +60,14 @@ resource "azurerm_storage_container" "curated" {
  
 }
 
+resource "azurerm_storage_container" "landing" {
+  
+  name                  = "landing"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+ 
+}
+
 resource "azurerm_storage_data_lake_gen2_filesystem" "synapse_filesystem" {
   name               = "benfownerstorage${random_string.random.result}"
   storage_account_id = azurerm_storage_account.storage.id
